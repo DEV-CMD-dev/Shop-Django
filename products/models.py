8359d940d625964d6e8119199cafbaf9cea3c7e5
Admin <overgeardev@gmail.com>
2025-12-10 22:33:12 +0200

    Add product model Add index template

--- products/models.py
+++ products/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Product(models.Model):
+    title = models.CharField(max_length = 80)
+    description = models.CharField()
+    price = models.PositiveIntegerField()
+    is_subscription = models.BooleanField(default = False)
+    duration_days = models.SmallIntegerField(null = True, blank = True)
+
+
+    def __str__(self):
+        return f"{self.title}"
