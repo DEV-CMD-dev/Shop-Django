d3089908cd0d21c5c1fcb10f7bbc4326985b8aab
Admin <overgeardev@gmail.com>
2026-01-16 22:05:51 +0200

    Add product form design  Fix redirect after adding product  Add product dislpay for admin

--- products/models.py
+++ products/models.py
@@ -1,9 +1,12 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 class Product(models.Model):
     title = models.CharField(max_length = 80)
-    description = models.CharField()
-    price = models.PositiveIntegerField()
+    description = models.CharField(max_length=200)
+    price = models.PositiveIntegerField(
+        validators = [MinValueValidator(1)]
+    )
     is_subscription = models.BooleanField(default = False)
     duration_days = models.SmallIntegerField(null = True, blank = True)
 
--- products/views.py
+++ products/views.py
@@ -17,7 +17,7 @@ def add_product(request):
         form = ProductForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect('products')
+            return redirect('products:list')
     else:
         form = ProductForm()
 
